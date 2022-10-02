14e34dca87698f29bb60859a4072d05e42c6023b
jatin kumar <jatins9227@gmail.com>
2022-10-02 15:02:09 +0530

    datepicker

--- js/dashboard.js
+++ js/dashboard.js
@@ -9,6 +9,32 @@ function showDropdown() {
   if ((openDropdown.style.display = "none")) {
     openDropdown.style.display = "unset";
   }
+}
 
 // popup btn
+function logoutbtn() {
+  document.querySelector(".logout-popup").style.display = "unset";
+}
+function closelogoutpopup() {
+  document.querySelector(".logout-popup").style.display = "none";
+}
 
+function buyApipopup() {
+  document.querySelector(".buy-api-popup").style.display = "unset";
+  document.querySelector(".background-overlay-popup").style.display = "unset";
+}
+
+function closeApipopup() {
+  document.querySelector(".buy-api-popup").style.display = "none";
+  document.querySelector(".background-overlay-popup").style.display = "none";
+}
+
+function extenddatepopup() {
+  document.querySelector(".extend-date-popup").style.display = "unset";
+  document.querySelector(".background-overlay-popup").style.display = "unset";
+}
+
+function closeextendDate() {
+  document.querySelector(".extend-date-popup").style.display = "none";
+  document.querySelector(".background-overlay-popup").style.display = "none";
+}
